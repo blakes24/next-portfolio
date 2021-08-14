23add6f3f41f9407b3f919f3162330d6d64006b7
blakes24 <blakes24@gmail.com>
2021-08-13 19:01:23 -0700

    Add about section

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import Head from 'next/head'
 import Link from "next/link";
+import About from "../components/About";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+    <main className={styles.main}>
+      <div className={styles.landing}>
         <h1 className={styles.title}>Blakely Burns</h1>
         <p className={styles.description}>Full-stack web developer</p>
         <Link href="/projects">
           <a className={styles.link}>View my work</a>
         </Link>
-      </main>
-    </div>
+      </div>
+      <About />
+    </main>
   );
 }
