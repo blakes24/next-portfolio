5e79b8c41ac0970848e40ef34767f079f87d1e2d
blakes24 <blakes24@gmail.com>
2021-10-09 17:41:48 -0700

    Add projects and move section above about

--- components/About.tsx
+++ components/About.tsx
@@ -18,27 +18,29 @@ const About: React.FC = () => {
     "cypress",
     "docker",
     "git",
-    "github",
+    "gitHub",
     "circleCI",
     "heroku",
   ];
   return (
     <section className={styles.main} id="about">
-      <h2>About Me</h2>
-      <div className={styles.bio}>
-        <img src="/images/profile-square.jpg" alt="Blakely Burns" />
-        <p>
-          I am a software developer and former science teacher with five years
-          of experience in education dedicated to helping students investigate
-          real-world problems and design creative solutions. I now enjoy
-          applying that same passion to solving problems through code.
-        </p>
-      </div>
-      <div className={styles.skills}>
-        <h2>Skills</h2>
-        {skills.map((skill) => (
-          <Icon skill={skill} key={skill} />
-        ))}
+      <div className={styles.container}>
+        <h2>About Me</h2>
+        <div className={styles.bio}>
+          <img src="/images/profile-square.jpg" alt="Blakely Burns" />
+          <p>
+            I am a software developer and former science teacher with five years
+            of experience in education dedicated to helping students investigate
+            real-world problems and design creative solutions. I now enjoy
+            applying that same passion to solving problems through code.
+          </p>
+        </div>
+        <div className={styles.skills}>
+          <h2>Skills</h2>
+          {skills.map((skill) => (
+            <Icon skill={skill} key={skill} />
+          ))}
+        </div>
       </div>
     </section>
   );
